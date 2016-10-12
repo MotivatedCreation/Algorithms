be376f3283bfc7c17c333a8d37e9994c5cd6c3a2
Jonathan Sullivan <jonsullivan@live.com>
2016-10-12 12:00:46 -0400

    Add files via upload

---
+++ Genetic%20Algorithm/Chromosome.vb
@@ -0,0 +1,14 @@
+﻿Public Class Chromosome
+
+    Public genes As String = ""
+    Public fitness As Integer = 0
+
+    Public Sub New(ByRef genes As String)
+        Me.genes = genes
+    End Sub
+
+    Overrides Function ToString() As String
+        Return "Genes: " + genes + "\nFitness: " + fitness
+    End Function
+
+End Class
